bf5c106268d399b0d3efc7c4b54824c521cfc22b
Alex Maitland <amaitland@users.noreply.github.com>
2022-11-03 13:09:52 +1000

    Basic example of using CefSharp.Wpf and CefSharp.Wpf.HwndHost in same application
    - app.manifest for HiDPI and GPU Acceleration
    - Enable crash reporting

---
+++ CefSharp.Mitel.Demo.Wpf/Behaviours/HoverLinkBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System;
+using Microsoft.Xaml.Behaviors;
+
+namespace CefSharp.Mitel.Demo.Wpf.Behaviours
+{
+    public class HoverLinkBehaviour : Behavior<FrameworkElement>
+    {
+        // Using a DependencyProperty as the backing store for HoverLink. This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HoverLinkProperty = DependencyProperty.Register("HoverLink", typeof(string), typeof(HoverLinkBehaviour), new PropertyMetadata(string.Empty));
+
+        public string HoverLink
+        {
+            get { return (string)GetValue(HoverLinkProperty); }
+            set { SetValue(HoverLinkProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            ((IWebBrowser)AssociatedObject).StatusMessage += OnStatusMessageChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            ((IWebBrowser)AssociatedObject).StatusMessage -= OnStatusMessageChanged;
+        }
+        
+        private void OnStatusMessageChanged(object sender, StatusMessageEventArgs e)
+        {
+            var browser = (FrameworkElement)sender;
+            browser.Dispatcher.BeginInvoke((Action)(() => HoverLink = e.Value));
+        }
+    }
+}
---
+++ CefSharp.Mitel.Demo.Wpf/Converter/TitleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CefSharp.Mitel.Demo.Wpf.Converter
+{
+    public class TitleConverter : IValueConverter
+    {
+#if DEBUG
+        private const string Build = "Debug";
+#else
+        private const string Build = "Release";
+#endif
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "CefSharp.Mitel.Demo.Wpf (" + Build + ") - " + (value ?? "No Title Specified");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
